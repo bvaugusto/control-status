40d5c7d72a92ab8b27a87c6576211f4c7b1a0a90
bvaugusto <bvaugusto@gmail.com>
2019-05-01 00:00:44 -0300

    Correção de import assets

--- control-status-front/src/main.js
+++ control-status-front/src/main.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
 import App from "./App";
 import VueResource from "vue-resource";
@@ -7,31 +5,15 @@ import VueResource from "vue-resource";
 import VueRouter from "vue-router";
 import { routes } from "./router";
 
-// import jquery from "vue-jquery";
 import Toasted from "vue-toasted";
-import "./plugins/assets";
-import store from "./store";
-
 import Vuetable from "vuetable-2";
 
-// import "jquery/dist/jquery.js";
-// import jQuery from "jQuery";
-// window.jQuery = jQuery;
-
-// window.$ = require('jquery')
-// window.JQuery = require('jquery')
-
-// import "jquery/dist/jquery.js";
-// import "bootstrap/dist/js/bootstrap.js";
-
-// import $ from "jquery";
-// window.jQuery = $;
-// window.$ = $;
+import "./plugins/assets";
+import store from "./store";
 
 Vue.config.productionTip = false;
 Vue.use(VueResource);
 Vue.use(VueRouter);
-// Vue.use(jquery);
 Vue.use(Toasted);
 
 Vue.use(Vuetable);
@@ -41,15 +23,6 @@ const router = new VueRouter({
   routes,
   mode: "history"
 });
-
-/* eslint-disable no-new */
-// new Vue({
-//   el: "#app",
-//   router,
-//   store,
-//   components: { App },
-//   template: "<App/>"
-// });
 
 new Vue({
   router,
