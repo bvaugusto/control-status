f31193d980a3658f646b471211ef4f43250006bd
bvaugusto <bvaugusto@gmail.com>
2019-05-02 04:12:08 -0300

    Removendo projeto front-bkp Alterando nome projeto frontend para front

--- control-status-front-bkp/src/plugins/assets.js
+++
@@ -1,8 +0,0 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "font-awesome/css/font-awesome.min.css";
-import "ionicons/dist/css/ionicons.min.css"
-
-import "admin-lte/dist/css/AdminLTE.min.css";
-import "admin-lte/dist/css/skins/_all-skins.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-import "admin-lte/dist/js/adminlte.min.js";
